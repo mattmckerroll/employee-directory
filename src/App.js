326ac92bf20a80be80816bbde660c2b5b1a99207
Matthew Mckerroll <matthew.mckerroll@gmail.com>
2021-02-27 22:47:30 -0500

    initialized react, added api, app, and table component code

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import FilterInput from "./FilterInput";
+import Table from "./Table";
+import { getUsersinfo } from "./API";
+import "./App.css";
+
+function App() {
+  const [initialUsers, updateAvailableUsers] = useState([]);
+  const [usersToRender, updateUsersToRender] = useState([]);
+
+  useEffect(() => {
+    getUsersinfo().then(({ data: { results } }) => updateAvailableUsers(results));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Employee Directory</h1>
+      <p>Filter the results by first name by entering something below!</p>
+      <FilterInput users={initialUsers} updateUsers={updateUsersToRender} />
+      <Table users={usersToRender} />
+    </div>
+  );
+}
+
+export default App;
